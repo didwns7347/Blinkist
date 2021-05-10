df93f1244d36bb8174cf5a8dd9dfc6b956df36a3
Kim jisu <Kim jisu@LAPTOP-EC2NBHEE.mshome.net>
2021-05-10 11:41:28 +0900

    Test

---
+++ src/main/java/com/markany/BlinkistApplication.java
@@ -0,0 +1,13 @@
+package com.markany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkistApplication.class, args);
+	}
+
+}
