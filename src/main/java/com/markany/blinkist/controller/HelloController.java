2a6dfbb305e290db60cc637e778d2390d58d3c75
MyCom <MyCom@115.23.163.199>
2021-05-10 14:50:47 +0900

    test1

---
+++ src/main/java/com/markany/blinkist/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.markany.blinkist.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller // String이나 ModelAndView객체반환
+public class HelloController {
+
+//메인화면
+	@GetMapping(value = "/")
+	public String hello(Model model) {
+
+		return "home";
+	}
+}
