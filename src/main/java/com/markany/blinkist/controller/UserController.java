e462f55560069181e638e27e4926d471a781bf49
MyCom <MyCom@115.23.163.199>
2021-05-13 14:40:09 +0900

    join form

--- src/main/java/com/markany/blinkist/controller/UserController.java
+++ src/main/java/com/markany/blinkist/controller/UserController.java
@@ -1,4 +1,3 @@
-  
 package com.markany.blinkist.controller;
 
 import java.io.IOException;
@@ -19,69 +18,74 @@ import com.markany.blinkist.vo.UserVo;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private NaverLoginService naverLoginService;
-	
+
 	@Autowired
 	UserService userService;
-	
 
-	//카카오톡 로그인 연동
+	// 카카오톡 로그인 연동
 	@RequestMapping("/kakaoLogin")
-	public String home(@RequestParam(value = "code", required = false) String code,HttpServletRequest request) throws Exception {
+	public String home(@RequestParam(value = "code", required = false) String code, HttpServletRequest request,
+			Model model) throws Exception {
 
 		KakaoService service = new KakaoService();
 		String access_Token = service.getAccessToken(code);
 		HashMap<String, Object> userInfo = service.getUserInfo(access_Token);
-		
-		//사용자가 입력한 email가져오기
-		String email = (String)userInfo.get("email");
-		
-		//user table에 헤당 이메일이 있는지 확인
-		UserVo user =  userService.findByEmail(email);
-		
+
+		// 사용자가 입력한 email가져오기
+		String email = (String) userInfo.get("email");
+
+		// user table에 헤당 이메일이 있는지 확인
+		UserVo user = userService.findByEmail(email);
+
 		// 이메일이 있다면 로그인후 볼수 있는 view 보여주기
-		if(user!=null) {
-			
-			//session저장
+		if (user != null) {
+
+			// session저장
 			HttpSession session = request.getSession();
-			session.setAttribute("user", email);
-			
+			session.setAttribute("authUser", email);
+
 			return "redirect:/";
-			
-		}else {	// 이메일이 없다면 회원가입시키기
-			
-	
+
+		} else { // 이메일이 없다면 회원가입시키기
+
+			model.addAttribute("email", email);
 			return "user/join";
-			
-			
+
 		}
 	}
-	
-	
-	//네이버로그인연동
+
+	// 네이버로그인연동
 	@RequestMapping("/login")
 	public String login(HttpSession session, Model model) {
-		String url=naverLoginService.getAuthorizationUrl(session);
+		String url = naverLoginService.getAuthorizationUrl(session);
 		model.addAttribute("url", url);
 		return "user/login";
 	}
-	
+
 	@RequestMapping(value = "/callback", method = { RequestMethod.GET, RequestMethod.POST })
 	public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session)
 			throws IOException, ParseException {
 		System.out.println("여기는 callback");
-		
+
 		OAuth2AccessToken oauthToken = naverLoginService.getAccessToken(session, code, state);
-        //로그인 사용자 정보를 읽어온다.
-	    String apiResult = naverLoginService.getUserProfile(oauthToken);
-	    String email=naverLoginService.getUserEmail(apiResult);
-		model.addAttribute("result", apiResult);
-		model.addAttribute("email", email);
-        /* 네이버 로그인 성공 페이지 View 호출 */
-		System.out.println(apiResult);
-		System.out.println("email= "+email);
-		return "redirect:/";
+		// 로그인 사용자 정보를 읽어온다.
+		String apiResult = naverLoginService.getUserProfile(oauthToken);
+		String email = naverLoginService.getUserEmail(apiResult);
+		UserVo user = userService.findByEmail(email);
+		// 이메일이 있다면 로그인후 볼수 있는 view 보여주기
+		if (user != null) {
+			session.setAttribute("authUser", email);
+			return "redirect:/";
+
+		} else { // 이메일이 없다면 회원가입시키기
+
+			model.addAttribute("email", email);
+			return "user/join";
+
+		}
+
 	}
 }
--- src/main/webapp/WEB-INF/jsp/user/join.jsp
+++ src/main/webapp/WEB-INF/jsp/user/join.jsp
@@ -1,12 +1,76 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
-<title>Insert title here</title>
+
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>대신해주세요 ${pageContext.request.contextPath }</title>
+
+<!-- Core theme CSS (includes Bootstrap)-->
+        <link href="${pageContext.request.contextPath }/css/styles.css" rel="stylesheet" />
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Custom styles for this template -->
+<link href="${pageContext.request.contextPath }/css/blog_home.css" rel="stylesheet">
+
+<title>로그인</title>
 </head>
 <body>
-afsdfafa
+	<!-- Navigation -->
+	<c:import url="/WEB-INF/jsp/include/navigation.jsp" />
+	<br/>
+	<div class="container">
+		<div class="container"><h2>회원가입</h2></div>
+		
+		<br></br>
+		<form action="${pageContext.request.contextPath }/user/join" method=post>
+			<div class="form-group">
+				<label for="usr">아이디:</label> <input type="text"
+					class="form-control" id="id" name="id">
+			</div>
+			<div class="form-group">
+				<label for="pwd">비밀번호:</label> <input type="password"
+					class="form-control" id="password" name="password">
+			</div>
+			<div class="form-group">
+				<label for="usr">이름:</label> <input type="text" class="form-control"
+					id="name" name="name">
+			</div>
+			<div class="form-group">
+				<label for="usr">E-mail:</label> <input type="text"
+					class="form-control" id="email" name="email">
+			</div>
+			<div class="form-group form-check">
+				<label class="form-check-label" for="radio1"> <input
+					type="radio" class="form-check-input" id="radio1" name="gender"
+					value="male" checked>남
+				</label>
+			</div>
+			<div class="form-group form-check">
+				<label class="form-check-label" for="radio2"> <input
+					type="radio" class="form-check-input" id="radio2" name="gender"
+					value="female">여
+				</label>
+			</div>
+			<button type="submit" class="btn btn-secondary">가입하기</button>
+		</form>
+	</div>
 </body>
 </html>
