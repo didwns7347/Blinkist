719870812c9b6c742f029adc4c2aa377690206c5
BIT <BIT@DESKTOP-5L6BMMO>
2021-07-06 10:18:18 +0900

    0706

--- src/main/java/com/markany/blinkist/controller/MailController.java
+++ src/main/java/com/markany/blinkist/controller/MailController.java
@@ -2,13 +2,11 @@ package com.markany.blinkist.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.markany.blinkist.service.MailSendService;
 
 
